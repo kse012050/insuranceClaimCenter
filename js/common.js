e3402363fbdf0737d4631be3b79cd7f8703511e9
김성은 <kse012050@naver.com>
2022-07-26 14:01:17 +0900

    상단 메뉴 모듈화... 어렵네

--- js/common.js
+++ js/common.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $('*').hasClass('sideMenu') && $('body').css('padding-bottom' , '50px');
-    $('*').hasClass('subPage') && $('.subPage').css('padding-top' , $('.subPage header').height() + 50);
+    // $('*').hasClass('subPage') && $('.subPage').css('padding-top' , $('.subPage header').height() + 50);
 
     // 메인 메뉴 클릭
     $('[data-event="menu"]').click(function(){
@@ -18,13 +18,16 @@ $(document).ready(function(){
     })
 
     // 스크롤 , 터치 이벤트
-    $(window).scroll(function(e){
-        if($(window).scrollTop() > 0){
-            $('.subPage header').addClass('active');
-        }else{
-            $('.subPage header').removeClass('active')
-        }
-    })
+
+    if($('.subPage').attr('data-event') === 'scroll'){
+        $(window).scroll(function(e){
+            if($(window).scrollTop() > 0){
+                $('.subPage header').addClass('active');
+            }else{
+                $('.subPage header').removeClass('active')
+            }
+        })
+    }
 
     console.log($('nav').hasClass('subPage'));
 })
