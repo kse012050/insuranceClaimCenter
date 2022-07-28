a0287fea3eecc6564ce9861cefc0394a5c47b515
김성은 <kse012050@naver.com>
2022-07-29 03:25:30 +0900

    scroll event 완성 추가적으로 더 만들고 있다

--- js/common.js
+++ js/common.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
     // click event
     clickEvent();
-
     // scroll event
     $('.subPage').attr('data-scroll') === 'fixed' && scrollEvent();
     
+    // 지원서비스 센터
+    supportMnue()
 })
 
 function clickEvent(){
@@ -15,6 +16,7 @@ function clickEvent(){
         clickAttr = $(this).attr('data-click');
         ((clickAttr === 'menu') || clickAttr.includes('popup')) && popupClick(clickAttr);
         clickAttr === 'drop' && dropEvent(e ,$(this).attr('data-drop'));
+        clickAttr === 'phoneConfirm' && $('[data-'+clickAttr+']').addClass('active');
     })
     
     menuSelector.click(function(){
@@ -44,6 +46,7 @@ function clickEvent(){
         ($(e.target).prop("tagName") === "BUTTON") && $(e.target).toggleClass('active');
             
     }
+
     
     $('form button').click((e)=>{
         e.preventDefault();
@@ -54,5 +57,40 @@ function scrollEvent(){
     $('header + *').css('paddingTop' , $('header').height())
     $(window).scroll(function(){
         $(window).scrollTop() > 0 ? $('header').addClass('active') : $('header').removeClass('active')
+    })
+}
+
+function supportMnue(){
+    let attrName;
+    let test = true;
+    $('.supportArea nav ul li a').click(function(e){
+        e.preventDefault();
+        attrName = $(this).attr('href');
+        // console.log($('#limit').offset().top);
+        // console.log(attrName);
+        // console.log($(''+attrName));
+        $('html').animate({'scrollTop' : $(attrName).offset().top - 150})
+        // console.log($('.supportArea nav').scrollLeft());
+        // console.log($('.supportArea nav').width());
+        // console.log($('.supportArea nav ul').width() + parseInt($('.supportArea nav ul').css('margin-left')) * 2);
+        // console.log('--------------------------------');
+    })
+
+    $(window).scroll(function(){
+        test = true;
+        $('.supportArea section').each(function(){
+            if($(this).offset().top - 150> $(window).scrollTop() && test){
+                attrName = $(this).attr('id');
+                $('.supportArea nav ul li').removeClass('active');
+                $('[href="#'+attrName+'"]').parent().addClass('active')
+                test = false;
+            }
+        });
+        // console.log(
+        //     $(window).scrollTop() / ($(document).height() - $(window).height())
+        // );
+        
+        // let test01 = (($('.supportArea nav ul').width() + parseInt($('.supportArea nav ul').css('margin-left')) * 2) - $('.supportArea nav').width()) * ($(window).scrollTop() / ($(document).height() - $(window).height()))
+        // $('.supportArea nav').scrollLeft(test01)
     })
 }
